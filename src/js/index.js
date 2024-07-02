cf0905d3f6067f60dd0f6990210c416321546503
Mishal Rai <minkumarrai@gmail.com>
2024-07-02 08:28:44 +0545

    added documentation file

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+import '../scss/style.scss';
+
+$(document).ready(function () {
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+            var offsetTop = $(hash).offset().top - 90;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: offsetTop
+            }, 200);
+        } // End if
+    });
+});
